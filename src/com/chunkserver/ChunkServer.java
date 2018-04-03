4b0028fc963b22c685c3cf961e10b8d9a6f53e7d
Nav Pillai <navneeth.pillai@gmail.com>
2018-04-03 14:25:22 -0700

    Added networking

--- src/com/chunkserver/ChunkServer.java
+++ src/com/chunkserver/ChunkServer.java
@@ -2,10 +2,15 @@ package com.chunkserver;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
-import java.util.Arrays;
-//import java.util.Arrays;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.Vector;
 
+import com.client.Client;
 import com.interfaces.ChunkServerInterface;
 
 /**
@@ -14,27 +19,60 @@ import com.interfaces.ChunkServerInterface;
  *
  */
 
-public class ChunkServer implements ChunkServerInterface {
+public class ChunkServer extends Thread implements ChunkServerInterface {
 	final static String filePath = "csci485/";	//or C:\\newfile.txt
 	public static long counter;
+	
+	private static ServerSocket serverSocket;
+	private static int port = 5656;
+	
+	private static ObjectOutputStream writeOut;
+	private static ObjectInputStream readIn;
+	
+	public static int PayloadSize = Integer.SIZE/Byte.SIZE;
+	public static int CommandSize = Integer.SIZE/Byte.SIZE; //size of each command code
+	
+	public static final int initCode = 100;
+	public static final int putCode = 101;
+	public static final int getCode = 102;
 	
 	/**
 	 * Initialize the chunk server
 	 */
 	public ChunkServer(){
-		File dir = new File(filePath);
-		File[] fs = dir.listFiles();
-
-		if(fs.length == 0){
+		File[] files = (new File(filePath)).listFiles();
+		if (files==null || files.length==0) {
 			counter = 0;
-		}else{
-			long[] cntrs = new long[fs.length];
-			for (int j=0; j < cntrs.length; j++)
-				cntrs[j] = Long.valueOf( fs[j].getName() ); 
-			
-			Arrays.sort(cntrs);
-			counter = cntrs[cntrs.length - 1];
 		}
+		else { //in case of deleted files, we need to make sure a new file is the next sequential one
+			Vector<Long> filenames = new Vector<Long>();
+			for (File f: files) {
+				if (isNumeric(f.getName())) //for hidden files such as .DS_Store on Mac 
+					filenames.add(Long.valueOf(f.getName()));
+			}
+			
+			if (filenames.size()==0)
+				counter = 0;
+			else {
+				Collections.sort(filenames);
+				counter = filenames.lastElement();
+			}
+		}
+		
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			return;
+		}
+	}
+
+	private boolean isNumeric(String s) {  
+		try {
+			Long l = Long.valueOf(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;		
 	}
 	
 	/**
@@ -85,7 +123,76 @@ public class ChunkServer implements ChunkServerInterface {
 			return null;
 		}
 	}
+		
+	public void run() {
+		Socket server = null;
+		while(true) {
+			try {
+				server = serverSocket.accept();			 				
+				writeOut = new ObjectOutputStream(server.getOutputStream());
+				readIn = new ObjectInputStream(server.getInputStream());
+				
+				while (true) {
+					int payloadlength = Client.readInt(readIn);
+					if (payloadlength==-1)
+						break;
+					
+					int code = Client.readInt(readIn); //which operation is being performed
+					if (code==initCode) {
+						String chunk = initializeChunk();
+						byte[] chunkHandlePayload = chunk.getBytes();
+						writeOut.writeInt(chunkHandlePayload.length);
+						writeOut.write(chunkHandlePayload);
+						writeOut.flush();
+					}
+					else if (code==putCode) {
+						int offset = Client.readInt(readIn);
+						int payloadsize = Client.readInt(readIn);
+						byte[] payload = Client.receivePayload(readIn, payloadsize);
+						int chunksize = payloadlength-ChunkServer.PayloadSize-ChunkServer.CommandSize-8-payloadsize;
+						byte[] chunkHandlePayload = Client.receivePayload(readIn, chunksize);
+						String chunk = new String(chunkHandlePayload);
+						
+						if (putChunk(chunk, payload, offset))
+							writeOut.writeInt(1);
+						else
+							writeOut.writeInt(0);
+						writeOut.flush();
+					}
+					else if (code==getCode) {
+						int offset = Client.readInt(readIn);
+						int payloadsize = Client.readInt(readIn);
+						int chunksize = payloadlength-ChunkServer.PayloadSize-ChunkServer.CommandSize-8;
+						byte[] chunkHandlePayload = Client.receivePayload(readIn, chunksize);
+						String chunk = new String(chunkHandlePayload);	
+						
+						byte[] output = getChunk(chunk, offset, payloadsize);
+						if (output==null)
+							writeOut.writeInt(ChunkServer.PayloadSize);
+						else {
+							writeOut.writeInt(ChunkServer.PayloadSize+output.length);
+							writeOut.write(output);
+						}
+						writeOut.flush();
+					}
+				}
+			     
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			} finally {
+				try {
+					if (server!=null)
+						server.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
-	
-
+	public static void main(String [] args) {
+		Thread t = new ChunkServer();
+		t.start();
+	}	
 }
--- src/com/client/Client.java
+++ src/com/client/Client.java
@@ -1,5 +1,10 @@
 package com.client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
 import com.chunkserver.ChunkServer;
 import com.interfaces.ClientInterface;
 
@@ -8,22 +13,47 @@ import com.interfaces.ClientInterface;
  * @author Shahram Ghandeharizadeh
  *
  */
-public class Client implements ClientInterface {
-	public static ChunkServer cs = new ChunkServer();
+public class Client implements ClientInterface {	
+	private static Socket client;
+	private static int port = 5656;
+	private static String hostname = "localhost";
+
+	private static ObjectOutputStream writeOut;
+	private static ObjectInputStream readIn;
 	
 	/**
 	 * Initialize the client
 	 */
 	public Client(){
-		if (cs == null)
-			cs = new ChunkServer();
+		if (client!=null)
+			return;
+		
+		try {
+			client = new Socket(hostname, port);	          
+			writeOut = new ObjectOutputStream(client.getOutputStream());
+			readIn = new ObjectInputStream(client.getInputStream());			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
 	 * Create a chunk at the chunk server from the client side.
 	 */
 	public String initializeChunk() {
-		return cs.initializeChunk();
+		try {
+			writeOut.writeInt(ChunkServer.CommandSize);
+			writeOut.writeInt(ChunkServer.initCode);
+			writeOut.flush();
+						
+			int chunkSize = readInt(readIn);
+			byte[] chunkHandlePayload = receivePayload(readIn, chunkSize);
+			return (new String(chunkHandlePayload));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	/**
@@ -34,7 +64,23 @@ public class Client implements ClientInterface {
 			System.out.println("The chunk write should be within the range of the file, invalide chunk write!");
 			return false;
 		}
-		return cs.putChunk(ChunkHandle, payload, offset);
+		try {
+			byte[] chunkHandlePayload = ChunkHandle.getBytes();
+			writeOut.writeInt(ChunkServer.PayloadSize+ChunkServer.CommandSize+4+4+payload.length+chunkHandlePayload.length);
+			writeOut.writeInt(ChunkServer.putCode);
+			writeOut.writeInt(offset);
+			writeOut.writeInt(payload.length);
+			writeOut.write(payload);
+			writeOut.write(chunkHandlePayload);
+			writeOut.flush();
+			int output = readInt(readIn);
+			if (output==0)
+				return false;
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 	/**
@@ -45,10 +91,53 @@ public class Client implements ClientInterface {
 			System.out.println("The chunk read should be within the range of the file, invalide chunk read!");
 			return null;
 		}
-		return cs.getChunk(ChunkHandle, offset, NumberOfBytes);
+		//return cs.getChunk(ChunkHandle, offset, NumberOfBytes);
+		try {
+			byte[] chunkHandlePayload = ChunkHandle.getBytes();
+			writeOut.writeInt(ChunkServer.PayloadSize+ChunkServer.CommandSize+4+4+chunkHandlePayload.length);
+			writeOut.writeInt(ChunkServer.getCode);
+			writeOut.writeInt(offset);
+			writeOut.writeInt(NumberOfBytes);
+			writeOut.write(chunkHandlePayload);
+			writeOut.flush();
+			int chunkSize = readInt(readIn) - ChunkServer.PayloadSize;
+			return receivePayload(readIn, chunkSize);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	
+	public static byte[] receivePayload(ObjectInputStream in, int size) {
+		byte[] tmp = new byte[size];
+		byte[] buffer = new byte[size];
+		int bytes = 0;
+		
+		while (bytes != size) {
+			int counter=-1;
+			try {
+				counter = in.read(tmp, 0, (size-bytes)); //read in a payload carefully
+				for (int j=0; j < counter; j++){
+					buffer[bytes+j]=tmp[j];
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+			if (counter == -1) {
+				return null;
+			}
+			else { 
+				bytes += counter;	
+			}
+		} 
+		return buffer;
+	}
 
-
+	public static int readInt(ObjectInputStream in) { //read one int in
+		byte[] buffer = receivePayload(in, 4);
+		if (buffer!=null)
+			return ByteBuffer.wrap(buffer).getInt();
+		return -1;
+	}
 }
